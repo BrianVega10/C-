d54d46e434fc26231677e2d6cf65580eb91165ed
BrianVega10 <88786103+BrianVega10@users.noreply.github.com>
2021-10-10 18:12:47 -0500

    Update 4444.cpp

--- 4444.cpp
+++ 4444.cpp
@@ -1,13 +1,3 @@
-#include <stdio.h>
-/* eco de los argumentos de la línea de órdenes; la. versión */
-main(int argc, char *argv[ ])
-{
-int i;
-for (i = 1; i < argc; i + + )
-printf("%s% s", argv[i], (i < argv—1) ? "":"");
-  printf ("\n");
-retum 0;
-}
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #include <stdio.h>
