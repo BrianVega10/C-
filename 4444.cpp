fbce41c307ecb8f6f02d504e2ff3ec0878f9e41d
BrianVega10 <88786103+BrianVega10@users.noreply.github.com>
2021-10-11 15:39:06 -0500

    Update 4444.cpp

--- 4444.cpp
+++ 4444.cpp
@@ -1,32 +1,4 @@
 
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
- #include <stdio.h>
-#include < string.h>
-#define MAXLINES 5000 /* máx # de líneas a ordenar */
-char *lineptr[MAXLINES]; /* apuntadores a líneas de texto */
-int rea dlines( char * lineptr [ ], int nlines);
-void writelines(char *lineptr[ ], int nlines);
-void qsort(void *lineptr[ ], int left, int right,
-int (*comp) (void *, void *));
-int numcmp(char *, char *);
-/* ordena líneas de entrada */
-main(int argc, char *argv[ ])
-f
-int nlines; /* número de lineas de entrada leídas */
-int numeric = 0; /* 1 si es ordenamiento numérico */
-if (argc > 1 && strcm p(argv[l], n") = = 0)
-numeric = 1;
-if ((nlines = readlines(lineptr, MAXLINES)) > = 0) {
-qsort((void **) lineptr, 0, nlines—1,
-(int (*) (void*, void*)) (numeric ? numemp : strcmp));
-writelines(lineptr, nlines);
-retum 0;
-} else {
-  printf ("entrada demasiado grande para ser ordenada\n");
-return 1;
-}
-}
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////7
 #include <math.h>
 /* numemp: compara si y s2 numéricamente */
